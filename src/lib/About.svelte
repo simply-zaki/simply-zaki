d459baf5d77dfa11410d3ffda86555fbc4ebb1fe
simply-zaki <thezakimain@gmail.com>
2024-08-30 07:22:13 +0100

    warning

--- src/lib/About.svelte
+++ src/lib/About.svelte
@@ -1,12 +1,25 @@
 <section id="about">
 	<img draggable="false" src="/cat.svg" alt="icon" />
 	<span>
+		<div>Website under construction 🚧</div>
 		<h1>Hello, im simply zaki</h1>
 		<p>A full stack web developer specializing in making user friendly websites and web apps. I create minimal easy to use sites that are helpful and make life easy without being confusing</p>
 	</span>
 </section>
 
 <style>
+div {
+    position: fixed;
+    right: 50%;
+    top: 50%;
+    transform: translate(50%, -50%);
+    background-color: black;
+    padding: 16px;
+    border-radius: 16px;
+    border: 1px solid var(--border);
+	z-index: 999;
+}
+
 	h1 {
 		font-size: 48px;
 		line-height: 1;
