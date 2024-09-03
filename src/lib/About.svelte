8118f9275ec19f2b64db98e079ad5a78f420cd3a
simply zaki <thezakimain@gmail.com>
2024-09-03 06:44:23 +0100

    removed icons from text

--- src/lib/About.svelte
+++ src/lib/About.svelte
@@ -3,7 +3,7 @@
 	<main>
 		<h1>Hello, im <span class="name">simply zaki</span></h1>
 		<p>
-			A full stack web developer<img src="/dev.svg" alt="icon" class="icon" />specializing in making user friendly<img src="/user_friendly.svg" alt="icon" class="icon" />websites and web apps<svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"></circle><line x1="2" y1="12" x2="22" y2="12"></line><path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"></path></svg>, I create minimal easy to use sites that are helpful and make life easy without being confusing<svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M14 9V5a3 3 0 0 0-3-3l-4 9v11h11.28a2 2 0 0 0 2-1.7l1.38-9a2 2 0 0 0-2-2.3zM7 22H4a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2h3"></path></svg>
+			A full stack web developer specializing in making user friendly websites and web apps, I create minimal easy to use sites that are helpful and make life easy without being confusing
 		</p>
 	</main>
 </section>
@@ -65,28 +65,7 @@
 
 	.name:hover {
 		font-family: 'Comic Sans MS', 'Comic Sans', cursive;
-		font-size: 0.95em; /* Slightly reduce font size */
-		font-weight: bold; /* Make it bold to compensate for size reduction */
-	}
-	.icon {
-		width: 24px;
-		height: 24px;
-		margin-right: 4px;
-		margin-left: 4px;
-		vertical-align: middle;
-		display: inline-block;
-		position: relative;
-		top: -1px;
-	}
-	@keyframes float {
-		0% {
-			transform: translateY(0px);
-		}
-		50% {
-			transform: translateY(-10px);
-		}
-		100% {
-			transform: translateY(0px);
-		}
+		font-size: 0.95em;
+		font-weight: bold;
 	}
 </style>
